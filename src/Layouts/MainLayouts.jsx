02ea9a3778226c14110a60dc917723383bb5ac8f
MernStackExpert <mdnirob30k@gmail.com>
2025-10-08 08:42:31 +0600

    Add Readme.md FIle

--- src/Layouts/MainLayouts.jsx
+++ src/Layouts/MainLayouts.jsx
@@ -1,23 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ToastContainer } from "react-toastify";
 import Navbar from "../Componants/Navbar";
-import { Outlet, useNavigation } from "react-router"; 
+import { Outlet, useLocation } from "react-router"; 
 import Footer from "../Componants/Footer";
 
 const MainLayouts = () => {
-  const navigation = useNavigation();
+  const location = useLocation();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 500);
+
+    return () => clearTimeout(timer);
+  }, [location]);
+
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
 
-      <main className="flex-grow">
-        {navigation.state === "loading" ? (
-          <div className="flex justify-center items-center h-[80vh]">
+      <main className="flex-grow relative">
+        {loading && (
+          <div className="absolute inset-0 flex justify-center items-center bg-white/70 backdrop-blur-sm z-50">
             <span className="loading loading-spinner loading-lg text-primary"></span>
           </div>
-        ) : (
-          <Outlet />
         )}
+
+        <Outlet />
       </main>
 
       <Footer />
